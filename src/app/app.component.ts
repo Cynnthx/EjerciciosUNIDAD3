d048538579e9b7383d2ff71f7c4ce3157c91d341
cynnthx <ccastelgonzalez@safareyes.es>
2024-10-25 17:51:52 +0200

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,24 @@ import {FechaYHoraActualComponent} from './fecha-yhora-actual/fecha-yhora-actual
 import {NumerosAleatoriosComponent} from './numeros-aleatorios/numeros-aleatorios.component';
 import {NavegacionHistorialComponent} from './navegacion-historial/navegacion-historial.component';
 import {InteraccionVentana1Component} from './interaccion-ventana1/interaccion-ventana1/interaccion-ventana1.component';
+import {InteraccionVentana2Component} from './interaccion-ventana2/interaccion-ventana2.component';
+import {ManipulacionCookiesComponent} from './manipulacion-cookies/manipulacion-cookies.component';
+import { SaludoUsuarioComponent } from './saludo-usuario/saludo-usuario.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, FechaYHoraActualComponent,
     NumerosAleatoriosComponent, NavegacionHistorialComponent,
-    InteraccionVentana1Component
+    InteraccionVentana1Component, InteraccionVentana2Component,
+    ManipulacionCookiesComponent, SaludoUsuarioComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
   title = 'EjerciciosUnidad3, ';
 }
